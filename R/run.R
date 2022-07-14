446f17bc7036cb51cf46cb998b64a30ba27337de
almac2022 <al@newgraphenvironment.com>
2022-07-13 18:09:15 -0700

    amend text to include more details and split the tables to allow better formatting

--- R/run.R
+++ R/run.R
@@ -5,8 +5,7 @@
 ##change your VErsion #
 #######################################################################################
 
-##we need to get our private data file moved over
-file.copy('C:/scripts/fish_passage_2021_permit_app_elk/R/private_info.R', 'R/private_info.R')
+
 
 ##move the html to the docs folder so it can be viewed online
 file.rename('application_moe.html', 'docs/index.html')
